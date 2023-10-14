f40ff96d3875c9c1deea05f114009b4f991b3480
Jose David <lujan9495@gmail.com>
2023-10-13 22:06:44 -0500

    Se agrega el proyecto

---
+++ README.md
@@ -0,0 +1,3 @@
+# rock-paper-scissors 
+
+In this project i am developing a rock, paper and scissors game, putting in practice HTML5, CSS3 and some JavaScript's logic. 
---
+++ main.js
@@ -0,0 +1,113 @@
+let playerAttack 
+let enemyAttack 
+let playerLifes = 3 
+let enemyLifes = 3
+
+function startGame(){
+    let resetGameButton = document.getElementById("reset-button")
+    resetGameButton.style.display = "none" 
+
+    let rockButton = document.getElementById("rock-button")
+    rockButton.addEventListener('click', rockAttack)  
+    let paperButton = document.getElementById("paper-button")
+    paperButton.addEventListener('click', paperAttack)
+    let scissorsButton = document.getElementById("scissors-button")
+    scissorsButton.addEventListener('click', scissorsAttack)
+
+    let resetButton = document.getElementById("reset-button")
+    resetButton.addEventListener("click", resetGame)
+}
+
+function rockAttack () {
+    playerAttack = "ROCK"
+    randomEnemyAttack()
+}
+
+function paperAttack () {
+    playerAttack = "PAPER"
+    randomEnemyAttack()
+}
+
+function scissorsAttack () {
+    playerAttack = "SCISSORS"
+    randomEnemyAttack()
+}
+
+function randomEnemyAttack () {
+    let randomAttack = random(1,3)
+
+    if (randomAttack == 1) {
+        enemyAttack = "ROCK"
+    } else if (randomAttack == 2) {
+        enemyAttack = "PAPER"
+    } else {
+        enemyAttack = "SCISSORS"
+    }
+
+    combat()
+}
+
+function combat() {
+    let spanPlayerLifes = document.getElementById("player-lifes")
+    let spanEnemyLifes = document.getElementById("enemy-lifes")
+   
+    if ( enemyAttack == playerAttack ){
+        createMessage(" - TIE") 
+    } else if ( playerAttack == "ROCK" && enemyAttack == "SCISSORS" || playerAttack == "PAPER" && enemyAttack == "ROCK" || playerAttack == "SCISSORS" && enemyAttack == "PAPER" ) { 
+        createMessage(" - YOU WON")
+        enemyLifes --
+        spanEnemyLifes.innerHTML = enemyLifes 
+    } else {
+        createMessage(" - YOU LOST")
+        playerLifes --
+        spanPlayerLifes.innerHTML = playerLifes
+    } 
+
+    checkLifes()
+}
+
+function checkLifes () {
+    if (playerLifes == 0) {
+        createFinalMessage("YOU LOST :(")  
+    } else if (enemyLifes == 0) {
+        createFinalMessage("YOU WIN :)")
+    } 
+}
+
+function createMessage(result) {
+    let messagesSection = document.getElementById("messages")
+
+    let paragraph = document.createElement("p")
+    paragraph.innerHTML = "You attacked with " + playerAttack + ", Enemy attacked with " + enemyAttack + result
+
+    messagesSection.appendChild(paragraph)
+}
+
+function createFinalMessage(finalResult) {
+    let resetGameButton = document.getElementById("reset-button")
+    resetGameButton.style.display = "block"
+
+    let messagesSection = document.getElementById("messages")
+
+    let paragraph = document.createElement('p')
+    paragraph.innerHTML = finalResult
+    
+    messagesSection.appendChild(paragraph)
+
+    let rockButton = document.getElementById("rock-button")
+    rockButton.disabled = true 
+    let paperButton = document.getElementById("paper-button")
+    paperButton.disabled = true 
+    let scissorsButton = document.getElementById("scissors-button")
+    scissorsButton.disabled = true 
+}
+
+function random (min,max) {
+    return Math.floor(Math.random() * (max - min +1) +min) 
+} 
+
+function resetGame(){
+    location.reload()
+}
+
+window.addEventListener("load", startGame)
